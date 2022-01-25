a89bbca08f393b402393bd07bcdfecafccc271ed
nemashto <ne.mash.to@gmail.com>
2022-01-25 17:33:17 +0100

    added timeline

--- react-app/src/components/timeline.js
+++ react-app/src/components/timeline.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Skeleton from 'react-loading-skeleton';
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllPosts } from '../state/postSlice'
 
 
 export const Timeline = () => {
+    const dispatch = useDispatch()
+
+    const posts = useSelector(state => state.post.posts)
+
+    useEffect(() => {
+        (async() => {
+            await( dispatch(getAllPosts()))
+        })()
+    }, [dispatch])
+
+    console.log(posts)
     return (
         <div className="container col-span-2">
-            TIMELINE
+            {posts===undefined ?(
+                <Skeleton count={2} width={640} height={500} className="mb-5" />
+            ): posts.length===0 ?(
+                <p className="flex justify-center font-bold">Follow other people to see Photos</p>
+            ): posts? (
+                posts.map((content) => <p>{content.caption}</p>)
+            ): null
+            }
         </div>
     )
 }
--- react-app/src/state/postSlice.js
+++ react-app/src/state/postSlice.js
@@ -3,8 +3,17 @@ import { PostService } from "../common/PostService"
 
 const initialState = { posts: [] }
 
+export const getAllPosts = createAsyncThunk(
+    "posts/",
+    async() => {
+        const response = await(new PostService()).getAll()
+        const data = await response.json()
+        return data
+    }
+)
+
 export const newPostAction = createAsyncThunk(
-    "post/new",
+    "posts/new",
     async(base) => {
         const response = await(new PostService()).create(base)
         const data = await response.json()
@@ -19,6 +28,13 @@ const postSlice = createSlice({
     extraReducers: (builder) => {
         builder
             .addCase(newPostAction.fulfilled, (state, action) => {
+                if (action.payload.errors) {
+                    state.posts = []
+                } else {
+                    state.posts = action.payload
+                }
+            })
+            .addCase(getAllPosts.fulfilled, (state, action) => {
                 if (action.payload.errors) {
                     state.posts = []
                 } else {
