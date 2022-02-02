01eee176ec2918ba092106b8963ee51baa85e5f1
nemashto <ne.mash.to@gmail.com>
2022-02-02 12:18:02 +0100

    more comments button

--- react-app/src/components/Post/comments.js
+++ react-app/src/components/Post/comments.js
@@ -4,18 +4,37 @@ import { AddComment } from "./addComment";
 
 export const Comments = ({id, comments: allComments, posted, commentImput}) => {
     const [comments, setComments] = useState(allComments)
-    console.log(comments)
+    const [commentsSlice, setCommentsSlice] = useState(2)
+
+    const showNextComments = () => {
+        setCommentsSlice(commentsSlice + 3)
+    }
+
     return (
         <>
             <div className="p-4 pt-1 pb-4">
-                {comments && comments.map((comment) => (
+                {comments && comments.slice(0, commentsSlice).map((comment) => (
                     <div key={comment.id} className="mb-1">
                         <Link to={`/p/${comment.userName}`} >
                             <span className="mr-1 font-bold">{comment.userName}</span>
                         </Link>
                         <span>{comment.body}</span>
                     </div>
-                ))} 
+                ))}
+                { comments.length >= 3 && commentsSlice < comments.length && (
+                    <button
+                    className="text-sm text-gray-base mb-1 cursor-pointer focus:outline-none"
+                    type="button"
+                    onClick={showNextComments}
+                    onKeyDown={(event) => {
+                      if (event.key === 'Enter') {
+                        showNextComments();
+                      }
+                    }}
+                  >
+                    View more comments
+                  </button>
+                )}
             </div>
             <AddComment id={id} comments={comments} setComments={setComments} commentInput={commentImput} />
         </>
